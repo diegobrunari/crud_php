372e38beeca97b8162876d9f504ce95e48508d6c
Diego Brunari <diego.skp@hotmail.com>
2024-10-07 20:23:22 -0300

    feat: adicionei uma verificação das variaveis de ambientes e uso de dotenv

---
+++ README.md
@@ -0,0 +1,3 @@
+## CRUD Básico em PHP / MySQL / PDO ##
+
+O sistema possibilitará, através de uma interface web, que o usuário realize as ações de inserir novos dados, editar dados existentes, listar registros do banco de dados mysql e realizar a exclusão desses dados, quando necessário.
--- conexao.php
+++ conexao.php
@@ -1,9 +1,27 @@
 <?php
 
+require 'vendor/autoload.php'; // Carregue o autoload do Composer
+
+use Dotenv\Dotenv;
+
+// Carregar as variáveis do arquivo .env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('HOST', $_ENV['HOST']);
+define('PORT', $_ENV['PORT']);
+define('DBNAME', $_ENV['DBNAME']);
+define('ADMUSER', $_ENV['ADMUSER']);
+define('ADMPASS', $_ENV['ADMPASS']);
+
 try {
-    //Faz conexão com o banco de dados
-    $conectar = new PDO("mysql:host=localhost;port=3306;dbname=pdo;", "root", "");
+    // Faz conexão com o banco de dados
+    if (!isset($_ENV['HOST'], $_ENV['PORT'], $_ENV['DBNAME'], $_ENV['ADMUSER'], $_ENV['ADMPASS'])) {
+        die("Variáveis de ambiente não estão configuradas corretamente.");
+    } else {
+        $conectar = new PDO("mysql:host=" . HOST . ";port=" . PORT . ";dbname=" . DBNAME . ";", ADMUSER, ADMPASS);
+    }
 } catch (PDOException $e) {
-    //Caso ocorra algum erro
-    echo "Falha ao conectar com o bando de dados: " . $e->getMessage();
+    // Caso ocorra algum erro
+    echo "Falha ao conectar com o banco de dados: " . $e->getMessage();
 }
